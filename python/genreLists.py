be7b94732f0b249a12809eb5f3b8480948b0eb57
jamesli124 <jameswli2021@gmail.com>
2020-02-04 01:10:53 -0500

    Moviemaker page
    Added a dropdown list and some random python code cuz I'm lazy

---
+++ python/genreLists.py
@@ -0,0 +1,23 @@
+#this is just for doing repetative stuff with the list of genres
+f = open("genres.txt")
+fout = open("genres.out", "w")
+genres = []
+i = 0
+while i < 24:
+    print(i)
+    genre = f.readline().replace("\n", "")
+    print(genre)
+    genres.append(genre)
+    i+=1
+
+html = ""
+for x in genres:
+    y = x.lower()
+    line = '<option value="' + y 
+    line += '">' + x 
+    line += '</option>\n'
+    print(line)
+    html += line
+fout.write(html)
+f.close()
+fout.close()
