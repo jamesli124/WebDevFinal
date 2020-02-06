5c2948cb15817859818f5675a5c87e3282ed526f
jamesli124 <jameswli2021@gmail.com>
2020-02-06 09:50:42 -0500

    fixed some links

--- root/movieGenres/genrePageMaker.py
+++ root/movieGenres/genrePageMaker.py
@@ -16,7 +16,7 @@ while i < 18:
     genre = movieData[i][0]
     fileName = genre.lower() + ".html"
     fout = open(fileName, "w")
-    html = '<!DOCTYPE html>\n<html lang="en">\n<head>\n    <meta charset="UTF-8">\n    <meta name="viewport" content="width=device-width, initial-scale=1.0">\n    <meta http-equiv="X-UA-Compatible" content="ie=edge">\n    <link rel="stylesheet" href="../css/style.css">\n    <link rel="stylesheet" href="../css/moviePage.css">\n    <title>%s</title>\n</head>\n<body>\n    <header>\n        <a href="index.html">Home</a>\n        <a href="movieMachine.html">Movies</a>\n        <a href="tvMachine.html">TV Shows</a>\n        <a href="musicMachine.html">Music</a>\n    </header>\n    <div class="film">\n        <div id="%s" class="parallax">\n            <p>You got<br><strong>%s</strong></p>\n        </div>\n        <div class="bigFilmContent">\n            <p>What are the best movies in %s?</p>\n        </div>\n' % (genre, genre, genre, genre)
+    html = '<!DOCTYPE html>\n<html lang="en">\n<head>\n    <meta charset="UTF-8">\n    <meta name="viewport" content="width=device-width, initial-scale=1.0">\n    <meta http-equiv="X-UA-Compatible" content="ie=edge">\n    <link rel="stylesheet" href="../css/style.css">\n    <link rel="stylesheet" href="../css/moviePage.css">\n    <title>%s</title>\n</head>\n<body>\n    <header>\n        <a href="../index.html">Home</a>\n        <a href="../movieMachine.html">Movies</a>\n        <a href="../tvMachine.html">TV Shows</a>\n        <a href="../musicMachine.html">Music</a>\n    </header>\n    <div class="film">\n        <div id="%s" class="parallax">\n            <p>You got<br><strong>%s</strong></p>\n        </div>\n        <div class="bigFilmContent">\n            <p>What are the best movies in %s?</p>\n        </div>\n' % (genre, genre, genre, genre)
     j = 0
     while j < 3:
         movieTitle = movieData[i][j * 5 + 2]
@@ -25,7 +25,7 @@ while i < 18:
         rottenTomatoes = movieData[i][j * 5 + 6]
         html += '        <div id="%s" class="parallax">\n            <div class="movieInfo">\n                <p class="center"><strong>%s</strong></p>\n                <p>Metacritic: %s</p>\n                <p>IMDB: %s</p>\n                <p>Rotten Tomatoes: %s</p>\n            </div>\n        </div>        \n' % (genre.lower()+str(j+1), movieTitle, metacritic, IMDB, rottenTomatoes)
         j+=1
-    html += '    </div>\n    <footer>\n        <ul>\n            <li><a href="index.html">Menu</a></li>\n            <li>copyright &copy; 2020 J.A.N.  ·  all rights reserved</li>\n        </ul>\n    </footer>\n</body>\n</html>'
+    html += '    </div>\n    <footer>\n        <ul>\n            <li><a href="../index.html">Menu</a></li>\n            <li>copyright &copy; 2020 J.A.N.  ·  all rights reserved</li>\n        </ul>\n    </footer>\n</body>\n</html>'
     fout.write(html)
     fout.close()
     i+=1
