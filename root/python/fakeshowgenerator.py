402bd99bcce4beff5963b43838e7e893b2a69aba
jamesli124 <jameswli2021@gmail.com>
2020-02-09 14:27:38 -0500

    generated placeholder pages for tv genres

--- root/python/fakeshowgenerator.py
+++ root/python/fakeshowgenerator.py
@@ -1,13 +1,13 @@
 import random
 fout = open("fakeTVRecommendation.csv", "w")
-fout.write("Genre,GenreImage,Show1,Image1,imdb,Show2,Image2,imdb,Show3,Image3,imdb")
+fout.write("Genre,GenreImage,Show1,Image1,imdb,Show2,Image2,imdb,Show3,Image3,imdb\n")
 genres = ["Action","Adventure","Comedy","Crime","Drama","Fantasy","Historical","Horror","Mystery","Political","Romance","Satire","Science-Fiction","Thriller"]
 begin = ["Game","Pain","Mane","Same","Chain","Rain","Fame"]
-end = ["Homes","Thrones","Moans","Stones","Prones", "Melons"]
+end = ["Homes","Thrones","Moans","Stones","Prones", "Melons","Bones","Hoes"]
 images = ["https://commons.wikimedia.org/wiki/Category:Images_-_landscape_aspect_ratio#/media/File:Braess1.png", "https://upload.wikimedia.org/wikipedia/commons/9/9b/-BLMcareers_%%2821857337562%29.jpg", "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/A_farmhouse_at_Stow_Green%2C_nr_Horbling_Lincolnshire%2C_England.JPG/1280px-A_farmhouse_at_Stow_Green%2C_nr_Horbling_Lincolnshire%2C_England.JPG", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/1929_Singer_Six_at_Capel_Manor%2C_Enfield%2C_London%2C_England_1.jpg/1280px-1929_Singer_Six_at_Capel_Manor%2C_Enfield%2C_London%2C_England_1.jpg"]
 class Generators:
     def makeName():
-        name = begin[random.randint(0,6)] + " of " + end[random.randint(0,5)]
+        name = begin[random.randint(0,6)] + " of " + end[random.randint(0,7)]
         return name
 
     def makeImage():
@@ -15,8 +15,8 @@ class Generators:
         return image
 
     def makeScore():
-        score = str(float(random.randint(0,100))*0.1)
-        return score + "/10"
+        score = float(random.randint(0,100))*0.1
+        return "{:.1f}/10".format(score)
 
 i = 0
 while (i < 14):
---
+++ root/showGenres/genrePageMaker.py
@@ -0,0 +1,29 @@
+f = open("fakeTVRecommendation.csv")
+movieData = [[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+categories = f.readline().split(",")
+categories[10] = categories[10].replace("\n", "")
+i = 0
+print(categories)
+print(categories[10])
+while i < 14:
+    movieData[i] = f.readline().split(",")
+    movieData[i][10] = movieData[i][10].replace("\n", "")
+    i+=1
+f.close()
+
+i = 0
+while i < 14:
+    genre = movieData[i][0]
+    fileName = genre.lower() + ".html"
+    fout = open(fileName, "w")
+    html = '<!DOCTYPE html>\n<html lang="en">\n<head>\n    <meta charset="UTF-8">\n    <meta name="viewport" content="width=device-width, initial-scale=1.0">\n    <meta http-equiv="X-UA-Compatible" content="ie=edge">\n    <link rel="stylesheet" href="../css/style.css">\n    <link rel="stylesheet" href="../css/moviePage.css">\n    <link rel="stylesheet" href="../css/showGenres.css">\n    <title>%s</title>\n</head>\n<body>\n    <header>\n        <a href="../index.html">Home</a>\n        <a href="../movieMachine.html">Movies</a>\n        <a href="../tvMachine.html">TV Shows</a>\n        <a href="../musicMachine.html">Music</a>\n    </header>\n    <div class="film">\n        <div id="%s" class="parallax">\n            <p>You got<br><strong>%s</strong></p>\n        </div>\n        <div class="bigFilmContent">\n            <p>What are the best shows in %s?</p>\n        </div>\n' % (genre, genre.lower(), genre, genre)
+    j = 0
+    while j < 3:
+        movieTitle = movieData[i][j * 3 + 2]
+        IMDB = movieData[i][j * 3 + 4]
+        html += '        <div id="%s" class="parallax">\n            <div class="movieInfo">\n                <p class="center"><strong>%s</strong></p>\n                <p>IMDB: %s</p>\n            </div>\n        </div>        \n' % (genre.lower()+str(j+1), movieTitle,IMDB)
+        j+=1
+    html += '    </div>\n    <footer>\n        <ul>\n            <li><a href="../index.html">Menu</a></li>\n            <li>copyright &copy; 2020 J.A.N.  ·  all rights reserved</li>\n        </ul>\n    </footer>\n</body>\n</html>'
+    fout.write(html)
+    fout.close()
+    i+=1
