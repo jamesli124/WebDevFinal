283c3abdad8a437aeb9f7b1e799a6ec58732dee8
jamesli124 <jameswli2021@gmail.com>
2020-02-09 20:22:36 -0500

    A bunch of style stuff

--- root/css/movieCSSGenerator.py
+++ root/css/movieCSSGenerator.py
@@ -1,25 +1,25 @@
-f = open("movieData.csv")
-movieData = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+f = open("fakeTVRecommendation.csv")
+movieData = [[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
 categories = f.readline().split(",")
-categories[16] = categories[16].replace("\n", "")
+categories[10] = categories[10].replace("\n", "")
 i = 0
 print(categories)
-print(categories[16])
-while i < 18:
+print(categories[10])
+while i < 14:
     movieData[i] = f.readline().split(",")
-    movieData[i][16] = movieData[i][16].replace("\n", "")
+    movieData[i][10] = movieData[i][10].replace("\n", "")
     i+=1
 f.close()
 
-fout = open("movieGenres.css", "w")
+fout = open("showGenres.css", "w")
 i = 0
 text = ""
-while i < 18:
+while i < 14:
     genre = movieData[i][0].lower()
     genreImage = movieData[i][1]
     movieImage1 = movieData[i][3]
-    movieImage2 = movieData[i][8]
-    movieImage3 = movieData[i][13]
+    movieImage2 = movieData[i][6]
+    movieImage3 = movieData[i][9]
     text += "#%s {\n   background-image: url(%s);\n}\n\n" % (genre, genreImage)
     text += "#%s {\n   background-image: url(%s);\n}\n\n" % (genre + "1", movieImage1)
     text += "#%s {\n   background-image: url(%s);\n}\n\n" % (genre + "2", movieImage2)
